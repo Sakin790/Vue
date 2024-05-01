4e7e702e5639059fd23d55198cd59adfdd8e670e
Sakin790 <sakinislam79@gmail.com>
2024-05-01 18:23:34 +0600

    working

---
+++ 03_Tutorial/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <h1>{{ message }}</h1>
+  <h1>{{ message.split("").reverse().join("") }}</h1>
+  <p>Count is : {{counter.count}}</p>
+
+</template>
+<script>
+export default {
+  data() {
+    return {
+      message: 'Hello World!',
+      counter: {
+        count: 0
+      }
+    }
+  }
+}
+</script>
