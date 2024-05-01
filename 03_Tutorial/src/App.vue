2bb6abd278966ab5074b7d5bcc249866af1de38c
Sakin790 <sakinislam79@gmail.com>
2024-05-01 18:40:59 +0600

    data and methoods

--- 03_Tutorial/src/App.vue
+++ 03_Tutorial/src/App.vue
@@ -1,18 +1,18 @@
-<template>
-  <h1>{{ message }}</h1>
-  <h1>{{ message.split("").reverse().join("") }}</h1>
-  <p>Count is : {{counter.count}}</p>
-
-</template>
 <script>
 export default {
   data() {
     return {
-      message: 'Hello World!',
-      counter: {
-        count: 0
-      }
+      count: 0
+    }
+  },
+  methods: {
+    increment() {
+      this.count++
     }
   }
 }
 </script>
+
+<template>
+  <button @click="increment">Count is: {{ count }}</button>
+</template>
