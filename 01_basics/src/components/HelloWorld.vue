d17fe1c39685f73dde359fdbc4cf687cd364d1ee
Sakin790 <sakinislam79@gmail.com>
2024-05-01 09:32:58 +0600

    refactor

---
+++ 01_basics/src/components/HelloWorld.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { ref } from 'vue'
+
+defineProps({
+
+  data :{
+    name : String,
+    age : Number,
+    gender : String,
+
+  }
+
+})
+
+
+</script>
+
+<template>
+
+  <h1>{{age}}</h1>
+</template>
