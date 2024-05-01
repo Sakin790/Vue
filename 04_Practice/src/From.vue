292fea59b9635043ee4e6c2f3b584ce427105e58
Sakin790 <sakinislam79@gmail.com>
2024-05-01 21:37:33 +0600

    v-model

--- 04_Practice/src/From.vue
+++ 04_Practice/src/From.vue
@@ -2,13 +2,16 @@
 export default {
   data() {
     return {
-      text: "",
+      inputData: "",
     };
   },
 };
 </script>
 
 <template>
-  <input v-model="text" placeholder="Type here" />
-  <p>{{ text }}</p>
+  <div>
+    // v-model only work in text model , not radio or checkBok
+    <input  v-model ="inputData" placeholder="Enter Text" />
+    <h2>input  Value is : {{inputData}} </h2>
+  </div>
 </template>
