d8b3b5ef24d98a857bfa228bea708950593e29ed
Sakin790 <sakinislam79@gmail.com>
2024-05-02 09:06:09 +0600

    conditional

--- 04_Practice/src/Conditional.vue
+++ 04_Practice/src/Conditional.vue
@@ -3,7 +3,9 @@ export default {
   data() {
     return {
       awesome: true,
-      isLoggedIn: false, // Assuming login state is managed elsewhere
+      isLoggedIn: false, 
+      score : 100,
+      // score:50<100
     };
   },
   methods: {
@@ -15,9 +17,12 @@ export default {
 </script>
 
 <template>
-  <button @click="toggle">Toggle</button>
+
   <div>
     <h1 v-if="isLoggedIn">You are welcome!</h1>
     <h1 v-else>Go...! and login first</h1>
+    <h4 v-if="score<50">Score is More Than 50</h4>
+    <h4 v-if="score>50">Score is Less Than 50</h4>
+    <button @click="toggle">Toggle</button>
   </div>
 </template>
