e16b0ecbbeab12816c91ec71db4f6dfa7936761b
Sakin790 <sakinislam79@gmail.com>
2024-05-03 21:03:44 +0600

    routing

--- 05_Routing/src/main.js
+++ 05_Routing/src/main.js
@@ -1,20 +1,6 @@
 import { createApp } from "vue";
-import { createRouter, createWebHistory } from "vue-router";
 import "./style.css";
 import App from "./App.vue";
-import Home from "./view/Home.vue";
-import About from "./view/About.vue";
-import Contact from "./view/Contact.vue";
-
-const routes = [
-  { path: "/", component: Home },
-  { path: "/about", component: About },
-  { path: "/contact", component: Contact },
-];
-
-const router = createRouter({
-  history: createWebHistory(),
-  routes: routes,
-});
+import { router } from "./routes/Index";
 
 createApp(App).use(router).mount("#app");
