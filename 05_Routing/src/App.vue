f195af3eb6059af63d11c654750a1590d660e51c
Sakin790 <sakinislam79@gmail.com>
2024-05-03 12:52:16 +0600

    router@4

--- 05_Routing/src/App.vue
+++ 05_Routing/src/App.vue
@@ -1,37 +1,8 @@
+<script setup>
 
-<script>
-import Home from './components/Home.vue';
-import About from './components/About.vue';
-import NotFound from './components/NotFound.vue';
-
-
-const routes = {
-  '/': Home,
-  '/about': About
-}
-
-export default {
-  data() {
-    return {
-      currentPath: window.location.hash
-    }
-  },
-  computed: {
-    currentView() {
-      return routes[this.currentPath.slice(1) || '/'] || NotFound
-    }
-  },
-  mounted() {
-    window.addEventListener('hashchange', () => {
-		  this.currentPath = window.location.hash
-		})
-  }
-}
 </script>
 
 <template>
-  <a href="/">Home</a> |
-  <a href="/about">About</a> |
-  <a href="/non-existent-path">Broken Link</a>
-  <component :is="currentView" />
+  <h1>Heder</h1>
+  <RouterView/>
 </template>
--- 05_Routing/src/main.js
+++ 05_Routing/src/main.js
@@ -1,5 +1,20 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from "vue";
+import { createRouter, createWebHistory } from "vue-router";
+import "./style.css";
+import App from "./App.vue";
+import Home from "./view/Home.vue";
+import About from "./view/About.vue";
+import Contact from "./view/Contact.vue";
 
-createApp(App).mount('#app')
+const routes = [
+  { path: "/", component: Home },
+  { path: "/about", component: About },
+  { path: "/contact", component: Contact },
+];
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes: routes,
+});
+
+createApp(App).use(router).mount("#app");
