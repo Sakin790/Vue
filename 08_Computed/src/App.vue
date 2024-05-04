713f6d636bdbefc64f9d50d3116b92c2c0b4fea4
Sakin790 <sakinislam79@gmail.com>
2024-05-04 12:54:28 +0600

    8

---
+++ 08_Computed/src/App.vue
@@ -0,0 +1,24 @@
+
+<script>
+export default {
+  
+  data(){
+    a:10
+  },
+methods:{
+ 
+}
+
+}
+</script>
+<template>
+  <div> 
+    <h1>
+      Hello
+    </h1>
+  </div>
+</template>
+
+<style>
+  
+</style>
