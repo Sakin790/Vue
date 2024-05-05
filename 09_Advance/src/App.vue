09fda43bacedc7ac7cd9354df221b1929c36b312
Sakin790 <sakinislam79@gmail.com>
2024-05-05 09:51:11 +0600

    1st

---
+++ 09_Advance/src/App.vue
@@ -0,0 +1,34 @@
+<script>
+export default {
+  data() {
+    return {
+      age: 3,
+      score: 100,
+      life: 90,
+    };
+  },
+  computed: {
+    
+  },
+  methods: {
+    incrementScore() {
+      console.log("Increment Score");
+      this.score++;
+    },
+    decrementLife() {
+      console.log("Decrementing Life.....");
+      this.life--;
+    },
+  },
+};
+</script>
+
+<template>
+  <div>
+    <h1>Advance Vue {{ age }}</h1>
+    <h2>{{ score }}</h2>
+    <h2>{{ life }}</h2>
+    <button @click="incrementScore">In-Score</button>
+    <button @click="decrementLife">De-Life</button>
+  </div>
+</template>
